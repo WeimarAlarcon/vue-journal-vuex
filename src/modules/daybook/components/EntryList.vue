016ab16884cc32f02635dd660c5dc5f971d9c8e3
Weimar Alarcon <weimaralarcon55@gmail.com>
2024-03-30 17:06:57 -0400

    Primer Commit

---
+++ src/modules/daybook/components/EntryList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="entry-list-container">
+    <div class="px-2 pt-2">
+      <input type="text"
+        class="form-control"
+        placeholder="Buscar entradas"
+      />
+    </div>
+    <div class="entry-scrollarea">
+      <Entry v-for="item in 100" :key="item"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  components: {
+    Entry: defineAsyncComponent(() => import('./Entry.vue'))
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-list-container {
+  height: calc(100vh - 56px);
+  // background-color: #f8f9fa;
+  border-right: 1px solid #2c3e50;
+  // overflow-y: hidden;
+}
+.entry-scrollarea {
+  height: calc(100vh - 110px);
+  overflow-y: scroll;
+}
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(), // 123456879531
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Ex qui deserunt mollit duis do in laboris eu qui. Anim labore minim pariatur fugiat voluptate ipsum. Voluptate tempor aliqua anim culpa proident occaecat incididunt proident. Elit proident sunt nostrud sunt culpa do proident tempor et ad.',
+            picture: null, // https://
+        },
+        {
+            id: new Date().getTime() + 1000, // 123456879531
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Veniam id esse do occaecat. Ut in minim Lorem veniam sint quis fugiat ut in Lorem ullamco pariatur culpa laboris. Velit eu mollit voluptate enim voluptate. Amet nostrud irure veniam incididunt nulla dolor quis sit dolore ex.',
+            picture: null, // https://
+        },
+        {
+            id: new Date().getTime() + 2000, // 123456879531
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Est consectetur consequat ut ad id consequat ea eiusmod ut id. Exercitation laborum aliqua sunt deserunt minim. Consequat adipisicing consequat mollit consectetur ea laboris tempor dolore pariatur nostrud ullamco. Ad ea quis velit ex dolore id ex amet ullamco. Eu esse veniam qui aliquip pariatur aliqua irure.',
+            picture: null, // https://
+        }
+    ],
+})
